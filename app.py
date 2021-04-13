abacafb9de369ecd4da06f367ac973bfc2db5071
Jay Shepherd <69314244+DevJayShepherd@users.noreply.github.com>
2021-04-13 13:30:24 +0100

    Version 1
    V.1

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+import datetime
+
+
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient('mongodb+srv://admindb:UlvbCECQa5VZKWFb@cluster0.bwcqx.mongodb.net/test')
+    app.db = client.streamshare
+
+    @app.route('/', methods=['GET', 'POST'])
+    def first_page():
+
+        if request.method == 'POST':
+            entry = request.form.get('content')
+
+            formatted_date = datetime.datetime.today().strftime('%Y-%m-%d')
+            app.db.entries.insert({"content": entry, "date": formatted_date})
+
+        entries_with_date = [
+            (entry["content"], entry["date"], datetime.datetime.strptime(entry["date"], '%Y-%m-%d').strftime('%b %d'))
+
+            for entry in app.db.entries.find({})
+        ]
+
+        return render_template('home.html', entries=entries_with_date)
+
+    return app
